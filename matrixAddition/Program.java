83fe3cefab22c5688faf4200b63056e77a310b75
not-josue <129870578+not-josue@users.noreply.github.com>
2023-11-14 01:49:37 -0500

    added Matrix Addition

---
+++ matrixAddition/Program.java
@@ -0,0 +1,123 @@
+/*
+ * A program that creates and adds matrices if they are the same length, and width
+ * By: Josue
+ */
+import java.util.Scanner;
+public class Program {
+
+	// constants
+
+	public static final int NUMBEROFMATRICES = 2;
+
+	// methods
+
+	// method for creating a multi-dimensional array/matrix
+	static int[][] createMatrix(int length, int width, Scanner sc) {
+		// initialize a local array
+		int[][] matrix = new int[length][width];
+		// iterate
+		for (int i = 0; i < length; i++)
+		{
+			for (int j = 0; j < matrix[i].length; j++)
+			{
+				System.out.println("Enter value at index " + i + " " + j);
+				matrix[i][j] = sc.nextInt();
+				sc.nextLine(); // fix-up
+			}
+		}
+		return matrix;
+	}
+
+	// method for checking if the length and width of 2 matrices is the same
+	static boolean sameSize(int[][] firstMatrix, int[][] secondMatrix) {
+		// initialize variables to hold the first Matrix's length and width
+		int firstLength = firstMatrix.length;
+		int firstWidth = firstMatrix[0].length;
+
+		// initialize variables to hold the second Matrix's length and width
+		int secondLength = secondMatrix.length;
+		int secondWidth = secondMatrix[0].length;
+
+		if ((firstLength == secondLength) && (firstWidth == secondWidth))
+			return true;
+
+		return false;
+	}
+
+	// method for adding 2 multi-dimensional arrays/matrices
+	static int[][] addMatrices(int[][] firstMatrix, int[][] secondMatrix) {
+		// initialize a local array
+		int[][] sumMatrix = new int[firstMatrix.length][firstMatrix[0].length];
+		// iterate
+		for (int i = 0; i < sumMatrix.length; i++)
+		{
+			for (int j = 0; j < sumMatrix[i].length; j++)
+			{
+				sumMatrix[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+			}
+		}
+		return sumMatrix;
+	}
+
+	// method for printing an array/matrix
+	static void printMatrix(int[][] matrix) {
+		// iterate
+		for (int[] i : matrix)
+		{
+			for (int j : i)
+			{
+				System.out.print(j + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+
+		// create an instance of Scanner
+		Scanner sc = new Scanner(System.in);
+
+		// prompt user
+		System.out.println("Welcome to the matrix adder program!\n");
+
+		// get user input for first matrix
+		System.out.println("Please enter the LENGTH of the first matrix");
+		int length = sc.nextInt();
+		sc.nextLine(); // fix-up
+		System.out.println("Please enter the WIDTH of the first matrix");
+		int width = sc.nextInt();
+		sc.nextLine(); // fix-up
+		int[][] firstMatrix = createMatrix(length, width, sc);
+		System.out.println();
+
+		// get user input for second matrix
+		System.out.println("Please enter the LENGTH of the second matrix");
+		length = sc.nextInt();
+		sc.nextLine(); // fix-up
+		System.out.println("Please enter the WIDTH of the second matrix");
+		width = sc.nextInt();
+		sc.nextLine(); // fix-up
+		int[][] secondMatrix = createMatrix(length, width, sc);
+		System.out.println();
+
+		sc.close();
+
+		// check if matrices are the same length and width
+		if (!sameSize(firstMatrix, secondMatrix))
+		{
+			System.out.println("Invalid Dimensions. These cannot be added.");
+			System.exit(0);
+		}
+
+		// declare a matrix with the added values
+		int[][] sumMatrix = addMatrices(firstMatrix, secondMatrix);
+
+		// print the matrices that were created
+		printMatrix(firstMatrix);
+		System.out.println("+");
+		printMatrix(secondMatrix);
+		System.out.println("=");
+		printMatrix(sumMatrix);
+	}
+
+}
---
+++ matrixAddition/README.md
@@ -0,0 +1,74 @@
+# Matrix Addition
+
+## Objective
+
+Write a program that creates two matrices of whole numbers and then adds them!
+
+$$ {\left\lbrack \matrix{4 & 1 \cr 8 & 7} \right\rbrack} 
++ \left\lbrack \matrix{1 & 3 \cr 2 & 4} \right\rbrack
+= \left\lbrack \matrix{5 & 4 \cr 10 & 11} \right\rbrack
+$$
+
+## Requirements
+
+### Data Structure
+
+- Only use Arrays to represent the Matrices
+
+### Creating Matrices
+
+- We assume that our matrices only have only two dimensions (a 2D Array).
+- Users must be able to specify the sizes of each Matrix - both length and width of both.
+
+### Populating Matrices
+
+- Users must be prompted to enter the individual values of each matrix (called components).
+- They must be prompted value's position (the indices) is being entered.
+
+### Adding Matrices
+
+- The program must check to see if the length and width of one matrix matches the length and width of the other matrix. If they do not match, then the user must be prompted with an error message and the program must exit.
+- The program must create a resulting matrix that is the same length and width.
+- The program must add the values component wise and store the results in the resulting matrix.
+Example. If we assume we have two matrices, A and B, and a resulting matrix C, then C[0][0] = A[0][0] + B[0][0], C[0][1] = A[0][1] + B[0][1], etc.
+
+### Printing the Result
+
+- The contents of both matrices entered by the user and the contents of the resulting matrix must be shown to the user.
+- Components of each should be shown in their appropriate rows and columns.
+
+## Example Dialogue
+
+Welcome to the matrix adder program!  
+Please enter the length and with of the first matrix  
+2  
+2  
+Enter the value at index 0 0  
+4  
+Enter the value at index 0 1  
+1  
+Enter the value at index 1 0  
+8  
+Enter the value at index 1 1  
+7  
+
+Please enter the length and with of the second matrix  
+2  
+2  
+Enter the value at index 0 0  
+1  
+Enter the value at index 0 1  
+3  
+Enter the value at index 1 0  
+2  
+Enter the value at index 1 1  
+4  
+
+4 1  
+8 7  
++  
+1 3  
+2 4  
+=  
+5 4  
+10 11
